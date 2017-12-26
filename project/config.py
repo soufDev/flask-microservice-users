47c78354c06d57de8709641320f4f1f15d79cf27
soufDev <ait.akkache.soufiane@gmail.com>
2017-12-26 12:43:00 +0100

    add changes

---
+++ project/config.py
@@ -0,0 +1,28 @@
+# project/config
+import os
+
+
+class BaseConfig:
+    """Base configuration"""
+    DEBUG = False
+    TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class DevelopmentConfig(BaseConfig):
+    """Development Config"""
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
+
+class TestingConfig(BaseConfig):
+    """Testing Configuration"""
+    DEBUG = True
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
+
+
+class ProductionConfig(BaseConfig):
+    """Production configuration"""
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
