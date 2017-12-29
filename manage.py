0eeee5712cbe3bc5f167bcbe470097acf6bd8d55
soufDev <ait.akkache.soufiane@gmail.com>
2017-12-29 22:02:01 +0100

    add blueprint to the service

--- manage.py
+++ manage.py
@@ -5,18 +5,11 @@ import unittest
 
 from flask_script import Manager
 
-from project import app, db
+from project import create_app, db
+from project.api.models import User
 
-
+app = create_app()
 manager = Manager(app)
-
-
-@manager.command
-def recreate_db():
-    """Recreates a database."""
-    db.drop_all()
-    db.create_all()
-    db.session.commit()
 
 
 @manager.command
@@ -27,6 +20,14 @@ def test():
     if result.wasSuccessful():
         return 0
     return 1
+
+
+@manager.command
+def recreate_db():
+    """Recreates a database."""
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
 
 
 if __name__ == '__main__':
