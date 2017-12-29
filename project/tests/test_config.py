3b511fe2e7ad441724239f3e557a6eeaef1dc4dc
soufDev <ait.akkache.soufiane@gmail.com>
2017-12-29 21:21:16 +0100

    add some modifications

--- project/tests/test_config.py
+++ project/tests/test_config.py
@@ -28,6 +28,23 @@ class TestTestingConfig(TestCase):
         app.config.from_object('project.config.TestingConfig')
         return app
 
+    def test_app_is_testing(self):
+        self.assertTrue(app.config['SECRET_KEY'] == 'my_precious')
+        self.assertTrue(app.config['DEBUG'])
+        self.assertTrue(app.config['TESTING'])
+        self.assertFalse(app.config['PRESERVE_CONTEXT_ON_EXCEPTION'])
+        self.assertTrue(
+            app.config['SQLALCHEMY_DATABASE_URI'] ==
+            'postgres://postgres:postgres@users-db:5432/users_test'
+        )
+
+
+class TestProductionConfig(TestCase):
+
+    def create_app(self):
+        app.config.from_object('project.config.ProductionConfig')
+        return app
+
     def test_app_is_production(self):
         self.assertTrue(app.config['SECRET_KEY'] == 'my_precious')
         self.assertFalse(app.config['DEBUG'])
